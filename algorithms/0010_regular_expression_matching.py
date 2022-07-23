b3ccccbbf936a757b8b1ae51e0a7d1e732527550
zmeeust <danaanpost@gmail.com>
2022-07-23 11:52:13 +0300

    LeetCode problem 0010: Regular Expression Matching (Update)

--- algorithms/0010_regular_expression_matching.py
+++ algorithms/0010_regular_expression_matching.py
@@ -26,7 +26,6 @@ s contains only lowercase English letters.
 p contains only lowercase English letters, '.', and '*'.
 It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
 """
-import re
 
 
 class Solution(object):
@@ -36,7 +35,27 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        return re.search("^" + p + "$", s) is not None
+        cache = {}
+
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+            if i >= len(s) and j >= len(p):
+                return True
+            if j >= len(p):
+                return False
+
+            match = i < len(s) and (s[i] == p[j] or p[j] == '.')
+            if (j + 1) < len(p) and p[j + 1] == '*':
+                cache[(i, j)] = dfs(i, j + 2) or (match and dfs(i + 1, j))
+                return cache[(i, j)]
+            if match:
+                cache[(i, j)] = dfs(i + 1, j + 1)
+                return cache[(i, j)]
+            cache[(i, j)] = False
+            return False
+
+        return dfs(0, 0)
 
 
 if __name__ == '__main__':
